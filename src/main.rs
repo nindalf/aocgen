2c78c9354f7d07ba879a6904bb7edf6d249f6cc0
Krisna Sundarram <krishnasundarram@monzo.com>
2022-12-03 09:54:47 +0000

    Simplify dayN template and remove files.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use clap::Parser;
 use tinytemplate::TinyTemplate;
 
 static TEMPLATE: &str = include_str!("dayn.rs.tmpl");
-static FILES_RS: &str = include_str!("files.rs");
 
 /// Generate the Rust file for that day's Advent of Code challenge
 #[derive(Parser, Debug)]
@@ -31,11 +30,6 @@ fn main() -> Result<()> {
     if !rs_path.exists() {
         let rendered = rendered_template(&n)?;
         std::fs::write(rs_path, rendered)?;
-    }
-
-    let files_rs_path = src.join("files.rs");
-    if !files_rs_path.exists() {
-        std::fs::write(files_rs_path, FILES_RS)?;
     }
 
     let inputs = std::path::Path::new("inputs");
