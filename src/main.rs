bb2abf8e5157d17a747d591d832eb1acc03b2226
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-15 14:43:27 +0000

    Split main into fetch and submit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod fetch;
+mod submit;
+
 use std::{fs::File, io::BufReader, path::PathBuf};
 
 use anyhow::Result;
@@ -5,23 +8,6 @@ use clap::Parser;
 use scraper::{Html, Selector};
 use serde::Deserialize;
 use tinytemplate::TinyTemplate;
-
-#[derive(Deserialize, Debug)]
-struct LangConfig {
-    exec_file_paths: Vec<String>,
-    input_file_paths: Vec<String>,
-    test_input_file_paths: Vec<String>,
-    readme_file_paths: Vec<String>,
-}
-
-#[derive(Debug)]
-struct MaterialisedConfig {
-    template: String,
-    exec_file_paths: Vec<PathBuf>,
-    input_file_paths: Vec<PathBuf>,
-    test_input_file_paths: Vec<PathBuf>,
-    readme_file_paths: Vec<PathBuf>,
-}
 
 /// Generate the Rust file for that day's Advent of Code challenge
 #[derive(Parser, Debug)]
@@ -51,21 +37,11 @@ struct Context {
 }
 
 #[derive(Debug, Clone, clap::ValueEnum, serde::Serialize)]
-enum Language {
+pub(crate) enum Language {
     JS,
     Python,
     Rust,
 }
-
-static JS_TEMPLATE: &str = include_str!("../templates/js.tmpl");
-static PYTHON_TEMPLATE: &str = include_str!("../templates/python.tmpl");
-static RUST_TEMPLATE: &str = include_str!("../templates/rust.tmpl");
-
-static README_TEMPLATE: &str = include_str!("../templates/readme.tmpl");
-
-static JS_CONFIG: &str = include_str!("../configs/js_config.json");
-static PYTHON_CONFIG: &str = include_str!("../configs/python_config.json");
-static RUST_CONFIG: &str = include_str!("../configs/rust_config.json");
 
 fn main() -> Result<()> {
     let args = Args::parse();
@@ -86,174 +62,7 @@ fn main() -> Result<()> {
         language: args.language,
     };
 
-    let config = get_config(&context, args.config)?;
-
-    let real_input = fetch_real_input(&context)?;
-    write_to_files(&real_input, &config.input_file_paths)?;
-
-    let test_input = fetch_test_input(&context)?;
-    write_to_files(&test_input, &config.test_input_file_paths)?;
-
-    let readme = fetch_readme(&context)?;
-    write_to_files(&readme, &config.readme_file_paths)?;
-
-    write_to_files(&config.template, &config.exec_file_paths)?;
+    fetch::fetch_and_write(&context);
 
     Ok(())
-}
-
-fn write_to_files(content: &str, paths: &[PathBuf]) -> Result<()> {
-    for path in paths {
-        if path.exists() {
-            // file already exists, do nothing
-            continue;
-        }
-        if let Some(parent) = path.parent() {
-            if !parent.exists() {
-                std::fs::create_dir_all(parent)?;
-            }
-        }
-        std::fs::write(path, content)?;
-    }
-    Ok(())
-}
-
-fn get_config(context: &Context, path: Option<PathBuf>) -> Result<MaterialisedConfig> {
-    let config: LangConfig = if let Some(path) = path {
-        let config_file = File::open(path)?;
-        let reader = BufReader::new(config_file);
-        serde_json::from_reader(reader)?
-    } else {
-        let config_str = match context.language {
-            Language::JS => JS_CONFIG,
-            Language::Python => PYTHON_CONFIG,
-            Language::Rust => RUST_CONFIG,
-        };
-        serde_json::from_str(config_str)?
-    };
-
-    let mut tt = TinyTemplate::new();
-
-    let exec_file_template = match context.language {
-        Language::JS => JS_TEMPLATE,
-        Language::Python => PYTHON_TEMPLATE,
-        Language::Rust => RUST_TEMPLATE,
-    };
-    tt.add_template("exec_file", exec_file_template).unwrap();
-    let template = tt.render("exec_file", &context).unwrap();
-
-    let exec_file_paths = materialise_paths(config.exec_file_paths, context);
-    let test_input_file_paths = materialise_paths(config.test_input_file_paths, context);
-    let input_file_paths = materialise_paths(config.input_file_paths, context);
-    let readme_file_paths = materialise_paths(config.readme_file_paths, context);
-
-    Ok(MaterialisedConfig {
-        template,
-        exec_file_paths,
-        input_file_paths,
-        test_input_file_paths,
-        readme_file_paths,
-    })
-}
-
-fn fetch_readme(context: &Context) -> Result<String> {
-    let html = fetch_problem_page(context)?;
-    let re = regex::Regex::new(r"<main>(?s).*</main>").unwrap();
-    let main = re.find(&html).unwrap().as_str();
-    let problem_statement = html2md::parse_html(main);
-
-    let mut tt = TinyTemplate::new();
-    tt.add_template("readme", README_TEMPLATE)?;
-    let mut context = context.clone();
-    context.problem_statement = problem_statement;
-
-    let mut readme = tt.render("readme", &context).unwrap();
-    let replacements = [
-        (r"&#39;", "'"),
-        (r"&gt;", ">"),
-        (r"&lt;", "<"),
-        (r"&quot;", "\""),
-        (r"You can .*Mastodon.* this puzzle.\n", ""),
-    ];
-    for (re, replacement) in replacements {
-        let re = regex::Regex::new(re).unwrap();
-        readme = re.replace_all(&readme, replacement).into_owned();
-    }
-    Ok(readme)
-}
-
-fn fetch_test_input(context: &Context) -> Result<String> {
-    let html = fetch_problem_page(context)?;
-    let fragment = Html::parse_fragment(&html);
-    let code_selector = Selector::parse("code").unwrap();
-    let mut code_fragments: Vec<String> = fragment
-        .select(&code_selector)
-        .filter_map(|element| element.first_child())
-        .filter_map(|child| child.value().as_text())
-        .map(|text| text.to_string())
-        .collect();
-    code_fragments.sort_by_key(|a| a.len());
-    code_fragments
-        .pop()
-        .ok_or_else(|| anyhow::anyhow!("No code blocks found"))
-}
-
-fn fetch_problem_page(context: &Context) -> Result<String> {
-    let year = context.year;
-    let day = context.day;
-    let url = format!("https://adventofcode.com/{year}/day/{day}");
-    let client = reqwest::blocking::Client::new();
-    let cookie = match std::env::var("AOC_COOKIE") {
-        Ok(cookie) => cookie,
-        Err(_) => {
-            eprintln!("AOC_COOKIE environment variable not set.\nFind the cookie value from your browser and set it as an environment variable.\nexport AOC_COOKIE=<cookie>");
-            std::process::exit(1);
-        }
-    };
-    let request = client
-        .get(url)
-        .header("cookie", format!("session={cookie}"))
-        .build()?;
-    Ok(client.execute(request)?.text()?)
-}
-
-fn fetch_real_input(context: &Context) -> Result<String> {
-    let year = context.year;
-    let day = context.day;
-    let url = format!("https://adventofcode.com/{year}/day/{day}/input");
-    let client = reqwest::blocking::Client::new();
-    let cookie = std::env::var("AOC_COOKIE")?;
-    let request = client
-        .get(url)
-        .header("cookie", format!("session={cookie}"))
-        .build()?;
-    let resp = client.execute(request)?.text()?;
-    if resp.starts_with("Please don't repeatedly request this endpoint before it unlocks!") {
-        let time = time_to_unlock(context)?;
-        eprintln!(
-            "Input not available yet. Please wait until the challenge unlocks in {}",
-            time
-        );
-        std::process::exit(1);
-    }
-    Ok(resp)
-}
-
-fn materialise_paths(input: Vec<String>, context: &Context) -> Vec<PathBuf> {
-    let mut result = vec![];
-    for path in input {
-        let mut tt = TinyTemplate::new();
-        tt.add_template("temp", &path).unwrap();
-        result.push(PathBuf::from(tt.render("temp", &context).unwrap()));
-    }
-    result
-}
-
-fn time_to_unlock(context: &Context) -> Result<jiff::SignedDuration> {
-    let unlock_time = jiff::civil::date(context.year as i16, 12, context.day as i8)
-        .at(5, 00, 0, 0)
-        .intz("UTC")?;
-    let now = jiff::Zoned::now();
-
-    Ok(now.duration_until(&unlock_time))
 }
